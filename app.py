926d08bbae000b1041d5f44d6935ba9d788719db
Waldemar Frączek <waldekfraczek1@gmail.com>
2024-04-23 03:00:42 +0200

    Added name form

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # export FLASK_ENV=development
 # export FLASK_APP=app.py
+
+
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "nothing for now"
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 # Create a route decorator
@@ -36,4 +49,17 @@ def page_not_found(e):
     return render_template("500.html")
 
 
-# Ended on episode #4 - Start on 5th
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
+    return render_template("name.html",
+                           name=name,
+                           form=form)
