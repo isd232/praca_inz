9fa8a016cc893b0386c46ead4ac701fd1297a8b8
Waldemar Frączek <waldekfraczek1@gmail.com>
2024-04-27 05:17:08 +0200

    Added deleting users, password hashing and db migration

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # export FLASK_ENV=development
@@ -17,11 +19,12 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
 # New MySQL DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:yourpassword@localhost/users'
 # Secret Key
 app.config['SECRET_KEY'] = "secretkey"
 # Initialize The Database
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # Create Model
@@ -29,18 +32,83 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # Password Hashing
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Create A String
     def __repr__(self):
         return '<Name %r>' % self.name
 
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User Deleted Successfully!")
+
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("add_user.html",
+                               form=form,
+                               name=name,
+                               our_users=our_users)
+    except:
+        flash("There was a problem deleting user, try again")
+        return render_template("add_user.html",
+                               form=form,
+                               name=name,
+                               our_users=our_users)
+
+
 # Create a Form Class
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+    favorite_color = StringField("Favorite Color")
     submit = SubmitField("Submit")
+
+
+# Update Database Record
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update)
+        except:
+            flash("Error! Looks like there was a problem... Try again.")
+            return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update)
+    else:
+        return render_template("update.html",
+                               form=form,
+                               name_to_update=name_to_update,
+                               id=id)
 
 
 # Create a Form Class
@@ -56,12 +124,13 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.favorite_color.data = ''
         flash("User Added Successfully!")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html",
@@ -111,3 +180,11 @@ def name():
     return render_template("name.html",
                            name=name,
                            form=form)
+
+
+# Check if the executed file is the main program and not a module imported elsewhere
+if __name__ == '__main__':
+    # Set Flask configuration to development mode explicitly
+    app.config['ENV'] = 'development'
+    app.config['DEBUG'] = True
+    app.run()  # Start the Flask application
