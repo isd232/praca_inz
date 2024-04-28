85124e0ac0c58076fac8dd7305903a559f53f891
Waldemar Frączek <waldekfraczek1@gmail.com>
2024-04-28 05:23:23 +0200

    Added main blog posts page and individual post page

--- app.py
+++ app.py
@@ -47,8 +47,17 @@ class PostForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
-#
+@app.route('/posts')
+def posts():
+    # Grab all the posts from the database
+    posts = Posts.query.order_by(Posts.date_posted)
+    return render_template("posts.html", posts=posts)
 
+
+@app.route('/posts/<int:id>')
+def post(id):
+    post = Posts.query.get_or_404(id)
+    return render_template("post.html", post=post)
 
 
 # Add Post Page
